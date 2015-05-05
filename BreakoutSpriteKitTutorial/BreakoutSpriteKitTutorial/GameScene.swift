45ccc2464158d818bfaf0fd999a8419bbae99b8d
Lidia Chou <lidia.chou94@gmail.com>
2015-05-05 10:41:49 -0300

    Getting started

---
+++ BreakoutSpriteKitTutorial/BreakoutSpriteKitTutorial/GameScene.swift
@@ -0,0 +1,22 @@
+//
+//  GameScene.swift
+//  BreakoutSpriteKitTutorial
+//
+//  Created by Lidia Chou on 5/5/15.
+//  Copyright (c) 2015 Lidia Chou. All rights reserved.
+//
+
+import SpriteKit
+
+let BallCategoryName = "ball"
+let PaddleCategoryName = "paddle"
+let BlockCategoryName = "block"
+let BlockNodeCategoryName = "blockNode"
+
+class GameScene: SKScene {
+    
+    override func didMoveToView(view: SKView) {
+        super.didMoveToView(view)
+    }
+    
+}
